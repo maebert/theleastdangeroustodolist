170f234461f99c7a5615556747b2fae8c54f8c00
karitarr <karitarr@gmail.com>
2020-01-28 21:49:42 -0800

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,11 +1,21 @@
 export default {
     basic: [
         "Leave the house",
-        "Tell a joke",
+        "Tell someone a joke",
         "Change into something more comfortable",
         "Wash your hands for as long as it takes you to sing 'Happy Birthday'",
-        "Respond to an email",
-        "Throw away a single sock",
-        "Eat breakfast"
+        "Respond to one email",
+        "Throw away all the mismatched socks in your drawer",
+        "Eat breakfast",
+        "Wear your favorite jeans",
+        "Clean up your computer desktop",
+        "Clean your laptop screen",
+        "Schedule an appointment",
+        "Listen to a Podcast while cleaning",
+        "Comment something positive on a social media post",
+        "Send a selfie to a friend",
+        "Cut your nails or clean your ears",
+        "Watch one movie that people cringe when you tell them you haven't seen",
+        
     ]
 }
